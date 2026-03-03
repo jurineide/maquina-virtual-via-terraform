9548c5ed738d4ab54351adca6c610a8b2588a6bc
jurineide <jurineidesouza@gmail.com>
2026-03-03 18:28:55 -0300

    Update user_data script for AWS instance to include web server setup

--- mvec2/infra/terraform.tf
+++ mvec2/infra/terraform.tf
@@ -18,7 +18,12 @@ resource "aws_instance" "app_server" {
     ami = "ami-0786adace1541ca80"
     instance_type = "t3.micro"
     key_name = "par-de-chaves"
-
+    user_data = <<-EOF
+    #!/bin/bash
+    cd /Documentos/mvec2
+    echo "<h1>É isso ai </h1>" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF 
     tags = {
         Name = "testeDeployViaTerraform"
     }
